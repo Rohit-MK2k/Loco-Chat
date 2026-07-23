0c3e77d5f32885409ba2d2bc928066a4aac19e00
Rohit <rohitmalakar007@gmail.com>
2026-07-23 20:30:13 +0530

    [Feat (Provider)] Add Gemini AI provider implementation

---
+++ src/core/ai/providers/gemini.ts
@@ -0,0 +1,27 @@
+import { GoogleGenAI } from "@google/genai";
+import type { AiProvider, Conversation } from "./types.ts";
+
+export class GeminiProvider implements AiProvider {
+    private client: GoogleGenAI
+    id:string = "google"
+
+    constructor(
+        apiKey: string,
+    ){
+        this.client = new GoogleGenAI({ apiKey })
+    }
+
+    listModels(): string[] {
+        return ["gemini-3-flash-preview"]
+    }
+
+    generateReply = async (conversation: Conversation, model: string, stream: boolean = true)=> {
+        const text_response =  await this.client.interactions.create({
+            store: false,
+            model: model,
+            input: conversation,
+        })
+        
+        return text_response.output_text
+    }
+}
